3994c1c957892944dbd94cce402f279278e65642
Mohammed Aeraf <aeraf@outlook.com>
2025-02-01 10:56:33 +0530

    CourseList (with Props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import AboutUs from "./Components/AboutUs.tsx";
 import Cars from "./Components/Cars.tsx";
-import CourseList from "./Components/CourseList.tsx";
+import CourseListProps from "./Components/CourseListProps.tsx";
 import Vehicles from "./Components/Vehicles.tsx";
 
 function App() {
@@ -23,12 +23,40 @@ function App() {
     "Ninja",
   ];
 
+  let courses = [
+    {
+      id: 1,
+      title: "Advanced Web Design",
+      description: "HTML CSS Bootstrap Responsive Design",
+      duration: "60 days",
+    },
+    {
+      id: 2,
+      title: "React Frontend",
+      description: "JS TS React",
+      duration: "50 days",
+    },
+    {
+      id: 3,
+      title: "Adv Excel",
+      description: "Adv Excel",
+      duration: "30 days",
+    },
+    {
+      id: 4,
+      title: "Python",
+      description: "Python Advanced",
+      duration: "30 days",
+    },
+  ];
+
   return (
     <>
       <header>
         <h1>React Demos</h1>
       </header>
-      <Vehicles items={bikes} title="Bikes List"></Vehicles>
+      {/* <Vehicles items={bikes} title="Bikes List"></Vehicles> */}
+      <CourseListProps courseList={courses} />
     </>
   );
 }
---
+++ src/Components/CourseListProps.tsx
@@ -0,0 +1,40 @@
+import "./CourseList.css";
+
+interface Course {
+  id: number;
+  title: string;
+  duration: string;
+  description: string;
+}
+
+interface CourseListProps {
+  courseList: Course[];
+}
+
+const CourseList = (props: CourseListProps) => {
+  // Conditional Rendering
+  if (props.courseList.length == 0) {
+    return (
+      <div>
+        <h2 className="text-danger">Course List</h2>
+        <h6 className="text-primary">No Course available</h6>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2 className="text-danger">Course List</h2>
+      <ul className="list-group">
+        {props.courseList.map((course) => (
+          <li className="list-group-item" key={course.id}>
+            {course.title} - {course.duration} <br />
+            <span className="small text-danger">{course.description}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CourseList;
