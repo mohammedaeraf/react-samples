f8544544f58aeae10a5baf4d3a4dc8fd6a85c34a
Mohammed Aeraf <aeraf@outlook.com>
2025-02-01 09:57:52 +0530

    Assignment 4 - Soln

---
+++ src/Components/LoginStatus.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoginStatus() {
+  const isLoggedIn = true; // Change this manually to see different outputs
+
+  return (
+    <div>
+      <h2>{isLoggedIn ? "Welcome, User!" : "Please log in."}</h2>
+    </div>
+  );
+}
+
+export default LoginStatus;
