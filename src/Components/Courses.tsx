87292ae06e60b95d3204bd1d20bd8c5c3b37b0c4
Mohammed Aeraf <aeraf@outlook.com>
2025-02-11 11:02:32 +0530

    Delete Course

--- src/Components/Courses.tsx
+++ src/Components/Courses.tsx
@@ -10,11 +10,25 @@ interface Course {
 
 function Courses() {
   const API_URL = "https://67a75555203008941f674e2f.mockapi.io/courses";
+
   const [courses, setCourses] = useState<Course[]>([]);
+
   const fetchCourses = async () => {
     const response = await fetch(API_URL);
     const data = await response.json();
     setCourses(data);
+  };
+
+  const deleteCourse = async (id: string) => {
+    const request = {
+      method: "DELETE",
+    };
+    const response = await fetch(`${API_URL}/${id}`, request);
+    const data = response.json();
+    console.log(data);
+
+    const updatedCourses = courses.filter((course) => course.id != id);
+    setCourses(updatedCourses);
   };
 
   useEffect(() => {
@@ -35,6 +49,7 @@ function Courses() {
             <th scope="col">Id</th>
             <th scope="col">Title</th>
             <th scope="col">Description</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -43,6 +58,14 @@ function Courses() {
               <th scope="row">{course.id}</th>
               <td>{course.title}</td>
               <td>{course.description}</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteCourse(course.id)}
+                >
+                  🗑️ Delete
+                </button>
+              </td>
             </tr>
             // Use the user's id as the key for each list item
           ))}
